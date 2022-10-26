ec5db5f09bc9e7514ed2035e075d28f74431c76e
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2022-10-26 09:37:00 +0500

    INFRA: Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿//================================================
+//Copyright (c) Calition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================================
+
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+        [ApiController]
+        [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() => 
+           Ok("Hello Garry, the princess is in another castle");
+    }
+}
