029548d5f50b8a68aed2493843cbf143469db87f
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2022-10-26 11:14:11 +0500

    INFRA: Initialize Unit Test Project

---
+++ Sheenam.API.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+﻿//================================================
+//Copyright (c) Calition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================================
+
+namespace Sheenam.API.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
